767ee61d6f5faf2be12d92a8c25a1be8c49c7ecf
Artem Starostenko <artemstarostenko65@gmail.com>
2017-09-09 21:26:04 +0300

    Initial compute instance resource

---
+++ README.md
@@ -0,0 +1,4 @@
+pip install -r requirements.txt
+
+export TOKEN=$(gcloud config config-helper --format='value(credential.access_token)')
+export PROJECT=infra-13120
---
+++ resources/compute_instance.py
@@ -0,0 +1,33 @@
+import googleapiclient.discovery
+from jinja2 import Environment, FileSystemLoader
+import json
+import os
+import requests
+
+token = os.getenv('TOKEN')
+project = os.getenv('PROJECT')
+
+def list_instances():
+    compute = googleapiclient.discovery.build('compute', 'v1')
+    result = compute.instances().list(project=project, zone='europe-west1-b').execute()
+    return result['items']
+
+def get_info(url):
+    headers = {
+            'Authorization': 'Bearer {}'.format(token),
+    }
+    r = requests.get(url, headers=headers)
+    return json.loads(r.content)
+
+def get_disk_info(url):
+    disk = get_info(url)
+    return disk['sourceImage'].rsplit('/', 1)[-1], disk['type'].rsplit('/', 1)[-1]
+
+# print(get_disk_info(''))
+print(list_instances())
+
+# env = Environment(loader=FileSystemLoader('templates'))
+# template = env.get_template('compute_instance.j2')
+# template.globals['get_disk_info'] = get_disk_info
+# output_from_parsed_template = template.render(instances=list_instances())
+# print output_from_parsed_template
